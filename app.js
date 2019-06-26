c5f2a82042d496de2c47b79aab4a9c49780aa061
Saad Ahmed <dropheaven@live.com>
2019-06-25 23:49:34 -0400

    store DOM elements in variables. Add event listener to form and add a task after form submit

---
+++ app.js
@@ -0,0 +1,41 @@
+// Define 
+const form = document.getElementById('task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.getElementById('filter');
+const taskInput = document.getElementById('task');
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener('submit', addTask);
+}
+
+// addTask
+function addTask(e) {
+  e.preventDefault();
+
+  if (taskInput.value === '') {
+    return alert("Add a task");
+  }
+
+  // create list element
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.innerText = taskInput.value;
+
+  // create link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = `<i class="fas fa-trash"></i>`;
+
+  // Append link to li
+  li.appendChild(link);
+
+  // Append li to UL
+  taskList.appendChild(li);
+
+  // Clear new task input field
+  taskInput.value = '';
+}
